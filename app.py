18126ef5960fc6c431fceb6a53836cb1951b4dcb
ExoSteel <sjungxuan@gmail.com>
2025-07-27 18:37:14 +0800

    Added next.html

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
+@app.route("/next", methods=['POST', 'GET'])
+def next():
+    if request.method == 'POST':
+        email = request.form["email"]
+        return render_template("next.html", email=email)
+    else:
+        return render_template("next.html", email="You aren't supposed to do that")
+
 @app.route("/youareanidiot")
 def yaai():
     return render_template("yaai.html")
